73838a55c10882e2d247390b9caf6f00e33bc9b1
Sanketdas05 <sanketdas2005@gmail.com>
2026-04-21 21:08:11 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://blog-law.vercel.app/',
+  'https://blog-law.vercel.app',
 ];
 
 app.use(cors({
